122c839272db89010a8c42bde0fe9f76126fdae3
Max922991 <max43223@gmail.com>
2023-12-16 19:39:41 +0300

    Optimize SQL

---
+++ src/test/java/com/example/optimizedb/OptimizeDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.optimizedb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptimizeDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
